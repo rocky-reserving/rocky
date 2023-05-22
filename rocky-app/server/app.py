d155c0490f0775c973a24f1233110150f876546a
aaweaver-actuary <53792857+aaweaver-actuary@users.noreply.github.com>
2023-05-22 16:20:08 -0400

    moved python files into src/py folder

--- rocky-app/server/app.py
+++ rocky-app/server/app.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS, cross_origin
 
+import sys
+
+sys.path.append("../..")
+
 from ..rockycore import ROCKY
 
 app = Flask(__name__)
